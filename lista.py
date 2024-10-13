d61afc2fa2df8312cc7f5430538ad6cf0ab806f6
mander92 <mariomartinrodriguez92@gmail.com>
2024-10-13 13:31:38 +0200

    Listas en python

--- lista.py
+++ lista.py
@@ -27,6 +27,33 @@ numeros[0] = 1000 #Ahora en la lista en el índice 0 el valor será de 1000
 
 print(numeros)
 
+numero_a_buscar = 3000
+
+if numero_a_buscar in numeros:
+    print('El número se encuentra en la lista')
+else: 
+    print('El número no se encuentra en la lista')
+
+#Recuperar el índice de cierto elemento de la lista 
+indice = numeros.index(300)
+print(indice)
+
+#Redefinir la lista para tener en cuenta los valores originales 
+numeros =[100,200,300,400,500]
+
+#Recuperar una sublista = lista[índice_inicio, índice_final -1]
+
+valores_recuperados = numeros[0:3]
+print(valores_recuperados)
+
+#Recuperar una sublista solo indicando el indice final
+print(numeros[:2]) #Por default si no se espcidica inicio será el índice 0 
+
+#Realizar una copia completa 
+lista_copiada = numeros[:]
+print(lista_copiada)
+
+
 
 
 
