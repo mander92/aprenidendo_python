9836a0ef555d55939bb1e43ea2a6d81a4800df01
mander92 <mariomartinrodriguez92@gmail.com>
2024-10-21 20:51:35 +0200

    "Practicando python"

---
+++ maquina_de_snacks.py
@@ -0,0 +1,78 @@
+
+# salir = False
+# pedido = []
+
+# while salir == False:
+
+items = [
+{'id': 0,
+'nombre': 'Papas',
+    'precio': 30
+    },
+    {'id': 1,
+'nombre': 'Refresco',
+    'precio': 50
+    },
+    {'id': 0,
+'nombre': 'Sandwich',
+    'precio': 120
+    }
+]
+    
+print('Hola mundo')
+   
+    # print('Sancks Disponibles:')
+    # print('  Id: 0 -> Papas - Precio: 300')
+    # print('  Id: 1 -> Refresco - Precio: 50')
+    # print('  Id: 2 -> Sadwich - Precio: 120')
+
+    # print('Menu:')
+    # print('   1. Comprar')
+    # print('   2. Mostrar ticket')
+    # print('   3. Salir')
+
+    # option = int(input('Escoge una opción:'))
+
+    # if(option == 1):
+    #     number = int(input('¿Qué número quieres?: '))
+    #     pedido.__add__(items[number])
+    #     print(f'Has pedido el producto: {items[0]}')
+    # elif(option == 2):
+    #     total = 0  
+    #     print('''Tu pedido es:
+    #           {pedido}1
+    #           con un total de: 
+    #           ''')   
+    # elif(option == 3):
+    #     salir = True
+    #     print('Hasta pronto')
+    # else:
+    #     print('Introduce un número válido')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while option > 0 and option < 3:
+#     print('*** Máquina de Snacks ***')
+#     print('Sancks Disponibles:')
+#     print('  Id: 0 -> Papas - Precio: 300')
+#     print('  Id: 1 -> Refresco - Precio: 50')
+#     print('  Id: 2 -> Sadwich - Precio: 120')
+#     print('Menu:')
+#     print('   1. Comprar')
+#     print('   2. Mostrar ticket')
+#     print('   3. Salir')
